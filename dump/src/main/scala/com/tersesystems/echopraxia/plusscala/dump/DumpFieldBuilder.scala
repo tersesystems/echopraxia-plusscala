1f9365a4b56c18c3bac0e8a7a3fc612339feaf24
Will Sargent <will.sargent@gmail.com>
2022-08-09 20:46:33 -0700

    Add dump field builder (#18)

---
+++ dump/src/main/scala/com/tersesystems/echopraxia/plusscala/dump/DumpFieldBuilder.scala
@@ -0,0 +1,66 @@
+package com.tersesystems.echopraxia.plusscala.dump
+
+import com.tersesystems.echopraxia.api.Field
+import com.tersesystems.echopraxia.plusscala.api.FieldBuilder
+
+import scala.annotation.tailrec
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+trait DumpFieldBuilder extends FieldBuilder {
+  import DumpFieldBuilder.impl
+  
+  def dumpKeyValue[A](expr: A): Field = macro impl.keyValue[A]
+
+  def dumpValue[A](expr: A): Field = macro impl.keyValue[A]
+}
+
+object DumpFieldBuilder extends DumpFieldBuilder {
+  
+  private class impl(val c: blackbox.Context) {
+    import c.universe._
+
+    def keyValue[A: c.WeakTypeTag](expr: c.Tree): c.Tree = {
+      val tpeA: Type = implicitly[WeakTypeTag[A]].tpe
+      // taken from https://github.com/dwickern/scala-nameof
+      @tailrec def extract(tree: c.Tree): String = tree match {
+        case Ident(n) => n.decodedName.toString
+        case Select(_, n) => n.decodedName.toString
+        case Function(_, body) => extract(body)
+        case Block(_, expr) => extract(expr)
+        case Apply(func, _) => extract(func)
+        case TypeApply(func, _) => extract(func)
+        case _ =>
+          c.abort(c.enclosingPosition, s"Unsupported expression: ${expr}")
+      }
+      val name = expr match {
+        case Literal(Constant(_)) => c.abort(c.enclosingPosition, "Cannot provide name to static constant!")
+        case _ => extract(expr)
+      }
+            
+      q"""(${c.prefix}.keyValue($name, fb.ToValue[$tpeA]($expr)))"""
+    }
+
+    def value[A: c.WeakTypeTag](expr: c.Tree): c.Tree = {
+      val tpeA: Type = implicitly[WeakTypeTag[A]].tpe
+            // taken from https://github.com/dwickern/scala-nameof
+      @tailrec def extract(tree: c.Tree): String = tree match {
+        case Ident(n) => n.decodedName.toString
+        case Select(_, n) => n.decodedName.toString
+        case Function(_, body) => extract(body)
+        case Block(_, expr) => extract(expr)
+        case Apply(func, _) => extract(func)
+        case TypeApply(func, _) => extract(func)
+        case _ =>
+          c.abort(c.enclosingPosition, s"Unsupported expression: ${expr}")
+      }
+      val name = expr match {
+        case Literal(Constant(_)) => c.abort(c.enclosingPosition, "Cannot provide name to static constant!")
+        case _ => extract(expr)
+      }
+            
+      q"""(${c.prefix}.value($name, fb.ToValue[$tpeA]($expr)))"""
+    }
+  }
+
+}
