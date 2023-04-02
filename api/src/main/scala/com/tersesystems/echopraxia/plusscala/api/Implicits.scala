3ac835754f18cb113cf30c2fdd0109098ba60c0e
Will Sargent <will.sargent@gmail.com>
2023-04-02 13:49:26 -0700

    rich values (#31)

--- api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/Implicits.scala
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/Implicits.scala
@@ -1,12 +1,15 @@
 package com.tersesystems.echopraxia.plusscala.api
 
 import com.tersesystems.echopraxia.api.{
+  Field,
+  Value,
   Condition => JCondition,
   FieldBuilderResult => JFieldBuilderResult,
   Level => JLevel,
   LoggingContext => JLoggingContext
 }
 
+import java.util
 import java.util.stream
 import java.util.stream.Collectors
 
@@ -25,6 +28,73 @@ trait LowPriorityImplicits {
   final implicit class RichLevel(level: JLevel) {
     @inline
     def asScala: Level = Level.asScala(level)
+  }
+
+  final implicit class RichValue[A](value: Value[A]) {
+
+    def asObject: Value.ObjectValue = value.asInstanceOf[Value.ObjectValue]
+
+    def asArray: Value.ArrayValue     = value.asInstanceOf[Value.ArrayValue]
+    def asBoolean: Value.BooleanValue = value.asInstanceOf[Value.BooleanValue]
+    def asString: Value.StringValue   = value.asInstanceOf[Value.StringValue]
+
+    def asNumber[N <: Number with Comparable[N]: Numeric]: Value.NumberValue[N] = value.asInstanceOf[Value.NumberValue[N]]
+  }
+
+  final implicit class RichArrayValue(values: Value[util.List[Value[_]]]) {
+    def add(value: Value[_]): Value[util.List[Value[_]]] = {
+      val joinedFields = new util.ArrayList(values.raw())
+      joinedFields.add(value)
+      Value.array(joinedFields)
+    }
+
+    def +(value: Value[_]): Value[util.List[Value[_]]] = add(value)
+
+    def append(newValues: Traversable[Value[_]]): Value[util.List[Value[_]]] = {
+      val joinedValues = new util.ArrayList(values.raw())
+      for (f <- newValues) {
+        joinedValues.add(f)
+      }
+      Value.array(joinedValues)
+    }
+
+    def ++(values: Traversable[Value[_]]): Value[util.List[Value[_]]] = append(values)
+
+    def append(newValues: util.Collection[Value[_]]): Value[util.List[Value[_]]] = {
+      val joinedValues = new util.ArrayList(values.raw)
+      joinedValues.addAll(newValues)
+      Value.array(joinedValues)
+    }
+
+    def ++(values: util.Collection[Value[_]]): Value[util.List[Value[_]]] = append(values)
+  }
+
+  final implicit class RichObjectValue(value: Value[util.List[Field]]) {
+    def add(field: Field): Value[util.List[Field]] = {
+      val joinedFields = new util.ArrayList(value.raw)
+      joinedFields.add(field)
+      Value.`object`(joinedFields)
+    }
+
+    def +(field: Field): Value[util.List[Field]] = add(field)
+
+    def append(fields: Traversable[Field]): Value[util.List[Field]] = {
+      val joinedFields = new util.ArrayList(value.raw)
+      for (f <- fields) {
+        joinedFields.add(f)
+      }
+      Value.`object`(joinedFields)
+    }
+
+    def ++(fields: Traversable[Field]): Value[util.List[Field]] = append(fields)
+
+    def append(fields: util.Collection[Field]): Value[util.List[Field]] = {
+      val joinedFields = new util.ArrayList(value.raw)
+      joinedFields.addAll(fields)
+      Value.`object`(joinedFields)
+    }
+
+    def ++(fields: util.Collection[Field]): Value[util.List[Field]] = append(fields)
   }
 
   final implicit class RichFieldBuilderResult(result: JFieldBuilderResult) {
---
+++ api/src/test/scala/com/tersesystems/echopraxia/plusscala/api/ImplicitsSpec.scala
@@ -0,0 +1,96 @@
+package com.tersesystems.echopraxia.plusscala.api
+
+import com.tersesystems.echopraxia.api.{Field, Value}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+import java.util
+
+class ImplicitsSpec extends AnyFunSpec with BeforeAndAfterEach with Matchers {
+
+  trait AnimalFieldBuilder extends FieldBuilder {
+    // this must be a method so we can call super:
+    implicit def animalToValue: ToValue[Animal] = { a =>
+      ToObjectValue(keyValue("name" -> a.name), keyValue("color" -> a.color))
+    }
+  }
+
+  // Special case cat as an animal
+  trait CatFieldBuilder extends AnimalFieldBuilder {
+    override implicit val animalToValue: ToValue[Animal] = { animal =>
+      // call super method
+      val animalValue = super.animalToValue.toValue(animal).asObject
+      animal match {
+        case cat: Cat =>
+          animalValue.add(keyValue("goodCat" -> cat.goodCat))
+        case _ =>
+          animalValue
+      }
+    }
+  }
+
+  object CatFieldBuilder extends CatFieldBuilder
+
+  class Animal(val name: String, val color: String)
+  class Cat(name: String, color: String, val goodCat: Boolean) extends Animal(name, color)
+
+  describe("rich ObjectValue") {
+    it("should work with a field builder") {
+      val fb    = CatFieldBuilder
+      val cat   = new Cat("indra", "black", goodCat = true)
+      val field = fb.keyValue("cat", cat)
+
+      val objectValue: Value.ObjectValue = field.value().asObject
+      val catField                       = objectValue.raw()
+      catField.stream().anyMatch(_.name == "goodCat") must be(true)
+    }
+
+    it("should work with Seq") {
+      val fb                             = CatFieldBuilder
+      val cat                            = new Cat("indra", "black", goodCat = true)
+      val field                          = fb.keyValue("cat", cat)
+      val objectValue: Value.ObjectValue = field.value().asObject
+      val fields = Seq(
+        fb.string("stringField", "foo"),
+        fb.number("numField", 1L)
+      )
+      val extra = objectValue.append(fields).asObject.raw
+
+      extra.stream().anyMatch(_.name == "stringField") must be(true)
+    }
+
+    it("should work with util.Collections") {
+      val fb                             = CatFieldBuilder
+      val cat                            = new Cat("indra", "black", goodCat = true)
+      val field                          = fb.keyValue("cat", cat)
+      val objectValue: Value.ObjectValue = field.value().asObject
+      val fields: util.List[Field] = util.Arrays.asList(
+        fb.string("stringField", "foo"),
+        fb.number("numField", 1L)
+      )
+      val extra = objectValue.append(fields).asObject.raw
+
+      extra.stream().anyMatch(_.name == "stringField") must be(true)
+    }
+  }
+
+  describe("rich ArrayValue") {
+    it("should work with a field builder") {
+      val fb        = CatFieldBuilder
+      val cat       = new Cat("indra", "black", goodCat = true)
+      val cats      = Seq(cat)
+      val fields    = fb.array("array", cats).value.asArray.raw
+      val catValue  = fields.get(0).asObject
+      val catFields = catValue.raw
+      catFields.stream().anyMatch(f => f.name == "name" && f.value().raw == "indra") must be(true)
+    }
+
+    it("should append extra values") {
+      val arrayValue    = Value.array("one", "two")
+      val newArrayValue = arrayValue.add(Value.string("three"))
+      newArrayValue.raw().size() must be(3)
+    }
+  }
+
+}
