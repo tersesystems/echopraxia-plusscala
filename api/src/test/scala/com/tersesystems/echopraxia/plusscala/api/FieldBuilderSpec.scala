cee45912b11aa7528aa7ad2a25e546dbc74bae29
Will Sargent <will.sargent@gmail.com>
2023-04-03 17:31:11 -0700

    Add java ToValue mappings

---
+++ api/src/test/scala/com/tersesystems/echopraxia/plusscala/api/FieldBuilderSpec.scala
@@ -0,0 +1,74 @@
+package com.tersesystems.echopraxia.plusscala.api
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+import java.math.BigInteger
+
+class FieldBuilderSpec extends AnyFunSpec with Matchers {
+
+  describe("FieldBuilder") {
+
+    it("should work with java.lang.Byte") {
+      val fb   = FieldBuilder
+      val byte = java.lang.Byte.MIN_VALUE
+      fb.keyValue("byte", byte)
+    }
+
+    it("should work with java.lang.Short") {
+      val fb    = FieldBuilder
+      val short = java.lang.Short.MIN_VALUE
+      fb.keyValue("short", short)
+    }
+
+    it("should work with java.lang.Integer") {
+      val fb      = FieldBuilder
+      val integer = java.lang.Integer.valueOf(1)
+      fb.keyValue("int", integer)
+    }
+
+    it("should not die when given a null integer") {
+      val fb                         = FieldBuilder
+      val integer: java.lang.Integer = null
+      fb.keyValue("int", integer)
+    }
+
+    it("should work with java.lang.Long") {
+      val fb   = FieldBuilder
+      val long = java.lang.Long.valueOf(1)
+      fb.keyValue("long", long)
+    }
+
+    it("should work with java.lang.Float") {
+      val fb    = FieldBuilder
+      val float = java.lang.Float.valueOf(1)
+      fb.keyValue("float", float)
+    }
+
+    it("should work with java.lang.Double") {
+      val fb     = FieldBuilder
+      val double = java.lang.Double.valueOf(1)
+      fb.keyValue("double", double)
+    }
+
+    it("should work with java.lang.BigInteger") {
+      val fb         = FieldBuilder
+      val bigInteger = BigInteger.ZERO
+      fb.keyValue("bigInteger", bigInteger)
+    }
+
+    it("should work with java.lang.BigDecimal") {
+      val fb         = FieldBuilder
+      val bigDecimal = BigDecimal.valueOf(1)
+      fb.keyValue("bigDecimal", bigDecimal)
+    }
+
+    it("should work with java.lang.Boolean") {
+      val fb   = FieldBuilder
+      val bool = java.lang.Boolean.TRUE
+      fb.keyValue("bool", bool)
+    }
+
+  }
+
+}
