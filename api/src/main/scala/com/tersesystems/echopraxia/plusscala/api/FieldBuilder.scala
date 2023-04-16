cfb3d9d5848381d3fec2862961d375108214f1c0
Will Sargent <will.sargent@gmail.com>
2023-04-15 17:02:46 -0700

    Break out the primitives (#42)

--- api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/FieldBuilder.scala
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/FieldBuilder.scala
@@ -221,17 +221,21 @@ trait TupleFieldBuilder extends ValueTypeClasses with ListToFieldBuilderResultMe
 
   def value[V: ToValue](tuple: (Name, V)): Field
 
-  def string(tuple: (Name, String)): Field = value(tuple)
-
-  def number[N: ToValue: Numeric](tuple: (Name, N)): Field = value(tuple)
-
-  def bool(tuple: (Name, Boolean)): Field = value(tuple)
-
   def exception(tuple: (Name, Throwable)): Field = keyValue(tuple)
 
   def array[AV: ToArrayValue](tuple: (Name, AV)): Field = keyValue(tuple)
 
   def obj[OV: ToObjectValue](tuple: (Name, OV)): Field = keyValue(tuple)
+}
+
+trait PrimitiveTupleFieldBuilder extends TupleFieldBuilder {
+
+  def string(tuple: (Name, String)): Field = value(tuple)
+
+  def number[N: ToValue : Numeric](tuple: (Name, N)): Field = value(tuple)
+
+  def bool(tuple: (Name, Boolean)): Field = value(tuple)
+
 }
 
 /**
@@ -248,6 +252,34 @@ trait ArgsFieldBuilder extends ValueTypeClasses with ListToFieldBuilderResultMet
   // value
 
   def value[V: ToValue](key: Name, value: V): Field
+
+  // ------------------------------------------------------------------
+  // null
+
+  def nullField(name: Name): Field = keyValue(name, Value.nullValue())
+
+  // ------------------------------------------------------------------
+  // exception
+
+  def exception(ex: Throwable): Field = Field.value(FieldConstants.EXCEPTION, Value.exception(ex))
+
+  def exception(name: Name, ex: Throwable): Field = keyValue(name, ex)
+
+  // ------------------------------------------------------------------
+  // array
+
+  def array[AV: ToArrayValue](name: Name, value: AV): Field =
+    keyValue(name, ToArrayValue[AV](value))
+
+  // ------------------------------------------------------------------
+  // object
+
+  def obj[OV: ToObjectValue](name: Name, value: OV): Field =
+    keyValue(name, ToObjectValue[OV](value))
+
+}
+
+trait PrimitiveArgsFieldBuilder extends ArgsFieldBuilder {
 
   // ------------------------------------------------------------------
   // string
@@ -277,34 +309,10 @@ trait ArgsFieldBuilder extends ValueTypeClasses with ListToFieldBuilderResultMet
   // boolean
 
   def bool(name: Name, boolean: Boolean): Field = value(name, boolean)
-
-  // ------------------------------------------------------------------
-  // null
-
-  def nullField(name: Name): Field = keyValue(name, Value.nullValue())
-
-  // ------------------------------------------------------------------
-  // exception
-
-  def exception(ex: Throwable): Field = Field.value(FieldConstants.EXCEPTION, Value.exception(ex))
-
-  def exception(name: Name, ex: Throwable): Field = keyValue(name, ex)
-
-  // ------------------------------------------------------------------
-  // array
-
-  def array[AV: ToArrayValue](name: Name, value: AV): Field =
-    keyValue(name, ToArrayValue[AV](value))
-
-  // ------------------------------------------------------------------
-  // object
-
-  def obj[OV: ToObjectValue](name: Name, value: OV): Field =
-    keyValue(name, ToObjectValue[OV](value))
-
 }
 
-trait StringArgsFieldBuilder extends ArgsFieldBuilder with HasName {
+
+trait StringNameArgsFieldBuilder extends ArgsFieldBuilder with HasName {
   override type Name = String
 
   override def keyValue[V: ToValue](key: Name, value: V): Field = Field.keyValue(key, ToValue(value))
@@ -320,6 +328,9 @@ trait StringNameTupleFieldBuilder extends TupleFieldBuilder {
   override def value[V: ToValue](tuple: (Name, V)): Field = Field.value(tuple._1, ToValue(tuple._2))
 }
 
-trait FieldBuilder extends StringArgsFieldBuilder with StringNameTupleFieldBuilder
+trait FieldBuilder extends StringNameArgsFieldBuilder
+  with StringNameTupleFieldBuilder
+  with PrimitiveTupleFieldBuilder
+  with PrimitiveArgsFieldBuilder
 
 object FieldBuilder extends FieldBuilder
