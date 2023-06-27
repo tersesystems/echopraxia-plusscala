a27312fb07fb028bda5d828da95296632ec66d40
Will Sargent <will.sargent@gmail.com>
2023-06-27 08:47:41 -0700

    Update factories with field builders

--- flow/src/main/scala/com/tersesystems/echopraxia/plusscala/flow/FlowLoggerFactory.scala
+++ flow/src/main/scala/com/tersesystems/echopraxia/plusscala/flow/FlowLoggerFactory.scala
@@ -12,12 +12,27 @@ object FlowLoggerFactory {
     FlowLogger(core, fieldBuilder)
   }
 
+  def getLogger[FB <: FlowFieldBuilder](name: String, fieldBuilder: FB): FlowLogger[FB] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, name)
+    FlowLogger(core, fieldBuilder)
+  }
+
   def getLogger(clazz: Class[_]): FlowLogger[FlowFieldBuilder] = {
     val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
     FlowLogger(core, fieldBuilder)
   }
 
+  def getLogger[FB <: FlowFieldBuilder](clazz: Class[_], fieldBuilder: FB): FlowLogger[FB] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
+    FlowLogger(core, fieldBuilder)
+  }
+
   def getLogger: FlowLogger[FlowFieldBuilder] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
+    FlowLogger(core, fieldBuilder)
+  }
+
+  def getLogger[FB <: FlowFieldBuilder](fieldBuilder: FB): FlowLogger[FB] = {
     val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
     FlowLogger(core, fieldBuilder)
   }
