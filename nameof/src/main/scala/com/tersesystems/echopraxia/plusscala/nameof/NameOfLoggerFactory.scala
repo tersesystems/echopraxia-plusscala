5b070fea18d4de8aa1504c7216ea44adf7bcec6e
Will Sargent <will.sargent@gmail.com>
2023-06-27 08:49:45 -0700

    Update factories with field builders (#47)

--- nameof/src/main/scala/com/tersesystems/echopraxia/plusscala/nameof/NameOfLoggerFactory.scala
+++ nameof/src/main/scala/com/tersesystems/echopraxia/plusscala/nameof/NameOfLoggerFactory.scala
@@ -14,12 +14,27 @@ object NameOfLoggerFactory {
     new NameOfLogger(core, fieldBuilder)
   }
 
+  def getLogger[FB <: FieldBuilder](name: String, fieldBuilder: FB): NameOfLogger[FB] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, name)
+    new NameOfLogger(core, fieldBuilder)
+  }
+
   def getLogger(clazz: Class[_]): NameOfLogger[FieldBuilder] = {
     val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
     new NameOfLogger(core, fieldBuilder)
   }
 
+  def getLogger[FB <: FieldBuilder](clazz: Class[_], fieldBuilder: FB): NameOfLogger[FB] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
+    new NameOfLogger(core, fieldBuilder)
+  }
+
   def getLogger: NameOfLogger[FieldBuilder] = {
+    val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
+    new NameOfLogger(core, fieldBuilder)
+  }
+
+  def getLogger[FB <: FieldBuilder](fieldBuilder: FB): NameOfLogger[FB] = {
     val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
     new NameOfLogger(core, fieldBuilder)
   }
