f759377cac3d6627f8217acc2be3e69f317ff7ea
Will Sargent <will.sargent@gmail.com>
2024-03-16 12:10:57 -0700

    Rename ToValueAttribute to ToValueAttributes

--- api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/FieldConversionImplicits.scala
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/FieldConversionImplicits.scala
@@ -7,14 +7,14 @@ trait FieldConversionImplicits { self: ValueTypeClasses =>
 
   // Convert a tuple into a field.  This does most of the heavy lifting.
   // i.e logger.info("foo" -> foo) becomes logger.info(Field.keyValue("foo", ToValue(foo)))
-  implicit def tupleToField[TV: ToValue](tuple: (String, TV))(implicit va: ToValueAttribute[TV]): Field = newField(tuple._1, tuple._2)
+  implicit def tupleToField[TV: ToValue](tuple: (String, TV))(implicit va: ToValueAttributes[TV]): Field = newField(tuple._1, tuple._2)
 
   // Convert an object with implicit ToValue and ToName to a field.
   // i.e. logger.info(foo) becomes logger.info(Field.keyValue(ToName[Foo].toName, ToValue(foo)))
-  implicit def nameAndValueToField[TV: ToValue: ToName](value: TV)(implicit va: ToValueAttribute[TV]): Field =
+  implicit def nameAndValueToField[TV: ToValue: ToName](value: TV)(implicit va: ToValueAttributes[TV]): Field =
     newField(implicitly[ToName[TV]].toName(value), value)
 
   // Creates a field, this is private so it's not exposed to traits that extend this
   @inline
-  private def newField[TV: ToValue: ToValueAttribute](name: String, tv: TV): Field = Utils.newField(name, ToValue(tv))
+  private def newField[TV: ToValue: ToValueAttributes](name: String, tv: TV): Field = Utils.newField(name, ToValue(tv))
 }
