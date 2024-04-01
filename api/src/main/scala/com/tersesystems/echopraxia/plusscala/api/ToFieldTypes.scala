34ba3d7343ebe1e30d6bba9b0f0920b3b89abbd9
Will Sargent <will.sargent@gmail.com>
2024-03-31 20:09:20 -0700

    rename tofield (#84)
    Rename ToLog to ToField

---
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/ToFieldTypes.scala
@@ -0,0 +1,24 @@
+package com.tersesystems.echopraxia.plusscala.api
+
+import com.tersesystems.echopraxia.api.Value
+
+trait ToFieldTypes { self: ValueTypeClasses =>
+  // Provides easier packaging for ToName and ToValue
+  trait ToField[-TF] {
+    def toName: ToName[TF]
+    def toValue: ToValue[TF]
+  }
+
+  object ToField {
+    def apply[TF](nameFunction: TF => String, valueFunction: TF => Value[_]): ToField[TF] = new ToField[TF] {
+      override val toName: ToName[TF] = t => nameFunction(t)
+      override val toValue: ToValue[TF] = t => valueFunction(t)
+    }
+  }
+
+  // implicit conversion from a ToField to a ToValue
+  implicit def convertToFieldToValue[TL: ToField]: ToValue[TL] = implicitly[ToField[TL]].toValue
+
+  // implicit conversion from a ToField to a ToName
+  implicit def convertToFieldToName[TL: ToField]: ToName[TL] = implicitly[ToField[TL]].toName
+}
