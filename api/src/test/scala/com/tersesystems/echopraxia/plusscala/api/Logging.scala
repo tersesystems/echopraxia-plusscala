cfcc1094c67545629de39851d3f912a39077baf6
Will Sargent <will.sargent@gmail.com>
2024-04-05 09:23:30 -0700

    WIP

--- api/src/test/scala/com/tersesystems/echopraxia/plusscala/api/Logging.scala
+++ api/src/test/scala/com/tersesystems/echopraxia/plusscala/api/Logging.scala
@@ -1,8 +1,21 @@
 package com.tersesystems.echopraxia.plusscala.api
 
 import java.time.Instant
+import com.tersesystems.echopraxia.api.Value
+import com.tersesystems.echopraxia.api.Attributes
 
 // This trait should be extended for domain model classes
 trait Logging extends LoggingBase with HeterogeneousFieldSupport {
   implicit val instantToValue: ToValue[Instant] = instant => ToValue(instant.toString)
+
+
+  trait ToValueAndAbbreviate[T] extends ToValueAttributes[T] {
+    def after: Int
+    def toValue(v: T): Value[_] = Value.nullValue
+    def toAttributes(value: Value[_]): Attributes = {
+      val abbreviateAfter = AbbreviateAfter(after)
+      val attrs = abbreviateAfter.toAttributes(value)
+      attrs.plusAll(AsValueOnly.attributes)
+    }
+  }
 }
---
+++ api/src/test/scala/com/tersesystems/echopraxia/plusscala/api/ValueAttributeSpec.scala
@@ -0,0 +1,17 @@
+package com.tersesystems.echopraxia.plusscala.api
+
+import com.tersesystems.echopraxia.api.{Field, Value}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+
+class ValueAttributeSpec extends AnyFunSpec with BeforeAndAfterEach with Matchers {
+
+  describe("ToValueAttribute") {
+    it("should compose two different attributes") {
+
+
+    }
+  }
+
+}
