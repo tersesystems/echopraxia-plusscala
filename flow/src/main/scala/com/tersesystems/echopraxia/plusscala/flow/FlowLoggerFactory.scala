2df7ac97410b603cbab7810af38dcdb72da23111
Will Sargent <will.sargent@gmail.com>
2024-04-25 10:13:35 -0700

    enforce singleton (#112)
    Enforce singleton type in loggers

--- flow/src/main/scala/com/tersesystems/echopraxia/plusscala/flow/FlowLoggerFactory.scala
+++ flow/src/main/scala/com/tersesystems/echopraxia/plusscala/flow/FlowLoggerFactory.scala
@@ -6,34 +6,34 @@ import com.tersesystems.echopraxia.spi.CoreLoggerFactory
 object FlowLoggerFactory {
   val FQCN: String = classOf[DefaultFlowLoggerMethods[_]].getName
 
-  val fieldBuilder: FlowFieldBuilder = DefaultFlowFieldBuilder
+  val fieldBuilder: DefaultFlowFieldBuilder.type = DefaultFlowFieldBuilder
 
-  def getLogger(name: String): FlowLogger[FlowFieldBuilder] = {
+  def getLogger(name: String): FlowLogger[DefaultFlowFieldBuilder.type] = {
     val core = CoreLoggerFactory.getLogger(FQCN, name)
     FlowLogger(core, fieldBuilder)
   }
 
-  def getLogger[FB <: FlowFieldBuilder](name: String, fieldBuilder: FB): FlowLogger[FB] = {
+  def getLogger[FB <: FlowFieldBuilder with Singleton](name: String, fieldBuilder: FB): FlowLogger[FB] = {
     val core = CoreLoggerFactory.getLogger(FQCN, name)
     FlowLogger(core, fieldBuilder)
   }
 
-  def getLogger(clazz: Class[_]): FlowLogger[FlowFieldBuilder] = {
+  def getLogger(clazz: Class[_]): FlowLogger[DefaultFlowFieldBuilder.type] = {
     val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
     FlowLogger(core, fieldBuilder)
   }
 
-  def getLogger[FB <: FlowFieldBuilder](clazz: Class[_], fieldBuilder: FB): FlowLogger[FB] = {
+  def getLogger[FB <: FlowFieldBuilder with Singleton](clazz: Class[_], fieldBuilder: FB): FlowLogger[FB] = {
     val core = CoreLoggerFactory.getLogger(FQCN, clazz.getName)
     FlowLogger(core, fieldBuilder)
   }
 
-  def getLogger: FlowLogger[FlowFieldBuilder] = {
+  def getLogger: FlowLogger[DefaultFlowFieldBuilder.type] = {
     val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
     FlowLogger(core, fieldBuilder)
   }
 
-  def getLogger[FB <: FlowFieldBuilder](fieldBuilder: FB): FlowLogger[FB] = {
+  def getLogger[FB <: FlowFieldBuilder with Singleton](fieldBuilder: FB): FlowLogger[FB] = {
     val core = CoreLoggerFactory.getLogger(FQCN, Caller.resolveClassName)
     FlowLogger(core, fieldBuilder)
   }
