ca188189721be2bec6e9ffb738039d4cc74e9216
Will Sargent <will.sargent@gmail.com>
2024-04-26 08:17:42 -0700

    Create TryToNameImplicits and EitherToNameImplicits (#114)
    * Add more implicits to fieldbuilder base
    
    * fmt
    
    * Move the ToName

--- api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/NameTypeClass.scala
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/NameTypeClass.scala
@@ -4,6 +4,10 @@ import com.tersesystems.echopraxia.spi.FieldConstants
 
 import scala.annotation.implicitNotFound
 
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+
 /**
  * Add this trait to get access to the ToName type class.
  */
@@ -21,12 +25,34 @@ trait NameTypeClass {
 
     implicit val sourceCodeToName: ToName[SourceCode] = _ => SourceCode.SourceCode
 
-    implicit def optNameToName[T: ToName]: ToName[Option[T]] = (t: Option[Option[T]]) => implicitly[ToName[T]].toName(t.flatten)
-
     def apply[T: ToName](t: T): String = implicitly[ToName[T]].toName(Option(t))
   }
 }
 
 trait StringToNameImplicits extends NameTypeClass {
   implicit val stringToName: ToName[String] = _.orNull
+}
+
+trait OptionToNameImplicits extends NameTypeClass {
+  implicit def optionToName[T: ToName]: ToName[Option[T]] = (t: Option[Option[T]]) => implicitly[ToName[T]].toName(t.flatten)
+}
+
+trait EitherToNameImplicits extends NameTypeClass {
+  implicit def eitherToName[L: ToName, R: ToName]: ToName[Either[L, R]] = {
+    case Some(either) => either match {
+      case Left(l: L) => ToName(l)
+      case Right(r: R) => ToName(r)
+    }
+    case None => null
+  }
+}
+
+trait TryToNameImplicits extends NameTypeClass {
+  implicit def tryToName[T: ToName]: ToName[Try[T]] = {
+    case Some(t) => t match {
+      case Success(v: T) => ToName(v)
+      case Failure(e) => ToName(e)
+    }
+    case None => implicitly[ToName[T]].toName(None)
+  }
 }
