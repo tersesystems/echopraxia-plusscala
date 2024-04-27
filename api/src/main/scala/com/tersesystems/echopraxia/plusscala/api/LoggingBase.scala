742b9b3c8a15eb1b423eb7a60c86ca000772b24a
Will Sargent <will.sargent@gmail.com>
2024-04-27 09:21:34 -0700

    Add StringToNameImplicits back to LoggingBase

--- api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/LoggingBase.scala
+++ api/src/main/scala/com/tersesystems/echopraxia/plusscala/api/LoggingBase.scala
@@ -3,8 +3,11 @@ package com.tersesystems.echopraxia.plusscala.api
 /**
  * This is a trait that should be extended by domain logging traits.
  *
+ * This trait recognizes strings as meaningful names. If this is not what you want (for example you want enums or refined types), you can create your
+ * own trait extending `LoggingTypeClasses with LoggingToValueImplicits` with your own `ToName` implicits.
+ *
  * {{{
- *  trait Logging extends LoggingBase with StringToNameImplicits {
+ *  trait Logging extends LoggingBase {
  *    implicit val currencyToField: ToField[Currency] = ToField(_ => "currency", currency => ToValue(currency.getCurrencyCode))
  *
  *    implicit val priceToField: ToField[Price] = ToField(_ => "price", price => ToObjectValue(price.currency, "amount" -> price.amount))
@@ -26,11 +29,16 @@ package com.tersesystems.echopraxia.plusscala.api
  * }
  * }}}
  */
-trait LoggingBase
-    extends ValueTypeClasses
-    with NameTypeClass
-    with FieldTypeClass
-    with OptionToValueImplicits
-    with EitherToValueImplicits
-    with FutureToValueImplicits
-    with FieldConversionImplicits
+trait LoggingBase extends LoggingTypeClasses with LoggingToValueImplicits with StringToNameImplicits
+
+/**
+ * This trait aggregates all the type classes together for convenience.
+ */
+trait LoggingTypeClasses extends ValueTypeClasses with NameTypeClass with FieldTypeClass
+
+/**
+ * This trait aggregates ToValue and field conversion implicits for using in field conversion for convenience.
+ */
+trait LoggingToValueImplicits extends FieldConversionImplicits with OptionToValueImplicits with EitherToValueImplicits with FutureToValueImplicits {
+  this: ValueTypeClasses with NameTypeClass with FieldTypeClass =>
+}
