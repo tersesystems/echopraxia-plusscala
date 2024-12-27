9f899f92c966ad74ce90853f2a00b0cb5fee7096
Will Sargent <will.sargent@gmail.com>
2024-12-26 22:31:55 -0800

    simple logger (#126)
    * Add simple logger
    
    * fix

---
+++ simple/src/main/scala/com/tersesystems/echopraxia/plusscala/simple/LoggerFactory.scala
@@ -0,0 +1,15 @@
+package com.tersesystems.echopraxia.plusscala.simple
+
+import com.tersesystems.echopraxia.spi.{Caller, CoreLoggerFactory}
+
+object LoggerFactory {
+
+  def getLogger(): Logger = getLogger(Caller.resolveClassName())
+
+  def getLogger(clazz: Class[_]): Logger = getLogger(clazz.getName)
+
+  def getLogger(name: String): Logger = {
+    val core = CoreLoggerFactory.getLogger(classOf[Logger].getName, name)
+    new Logger(core)
+  }
+}
